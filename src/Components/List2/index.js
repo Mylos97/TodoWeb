5c0f424abba7de8624b7ca8375e2052f2475a063
Mylos97 <57527830+Mylos97@users.noreply.github.com>
2021-11-23 10:11:01 +0100

    more work

--- src/Components/List2/index.js
+++ src/Components/List2/index.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { FaEdit , FaTrashAlt}  from 'react-icons/fa'
+import { EditText } from 'react-edit-text';
+import { Popup } from '..';
+
 import './index.css';
+import 'react-edit-text/dist/index.css';
+import PopUp from '../Popup';
 
 const data = [
   {
@@ -19,16 +25,35 @@ const data = [
 
 const List2 = () => {
   const [items, setItems] = useState(data);
+  const [editAbleItem, setEditAbleItem] = useState(null)
 
-  function handleOnDragEnd(result) {
+
+  const handleOnDragEnd = (result) => {
     if (!result.destination) return;
 
     const tmpItems = Array.from(items);
     const [reorderedItem] = tmpItems.splice(result.source.index, 1);
     tmpItems.splice(result.destination.index, 0, reorderedItem);
-
     setItems(tmpItems);
   }
+
+  const deleteItem = (itemId) => {
+    var tmpItems = Array.from(items);
+    tmpItems = tmpItems.filter((item) => item.id !== itemId)
+    setItems(tmpItems)
+  }
+
+  const editItem = (itemId, string) => {
+      var tmpItems = Array.from(items)
+      tmpItems.map((item) => {
+          if(item.id === itemId){
+              item.name = string
+          }
+        return item
+      })
+      setItems(tmpItems)
+  }
+
 
   return (
     <div className="container">
@@ -41,9 +66,15 @@ const List2 = () => {
                     <Draggable key={id} draggableId={id} index={index}>
                       {(provided) => (
                         <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                          <p>
-                            { name }
-                          </p>
+                            <EditText 
+                            value={name} 
+                            onChange={(text) => editItem(id, text)} 
+                            style={{backgroundColor:'rgb(0,0,255)', fontSize:'2rem', textAlign:'center'}}
+                            readonly={(id !== editAbleItem)}
+                            onSave={() => setEditAbleItem(null)}
+                            />
+                            <FaEdit onClick={() => setEditAbleItem(id)}/>
+                            <FaTrashAlt onClick={() => deleteItem(id)}/>
                         </li>
                       )}
                     </Draggable>
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export {default as List} from  './List'
 export {default as Header} from './Header'
 export {default as List2} from  './List2'
+export {default as Popup} from  './Popup'
